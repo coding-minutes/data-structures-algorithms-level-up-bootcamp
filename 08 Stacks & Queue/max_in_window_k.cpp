239c0261cabb0abd37ba611a34edcbbe2a6a31ee
Bhavya Aggarwal <58501974+bhavya278@users.noreply.github.com>
2022-05-04 14:37:55 +0530

    Code With Simplified Approach
    We can either return ans or print in function as well

--- 08 Stacks & Queue/max_in_window_k.cpp
+++ 08 Stacks & Queue/max_in_window_k.cpp
@@ -1,57 +1,33 @@
+
 #include <iostream>
-#include<vector>
-#include<deque>
+#include <vector>
+#include <deque>
 using namespace std;
 
-void maxSubArrayK(vector<int> a,int k){
-
-    //Algorithm
-    int n = a.size();
-
-    deque<int> Q(k);
-    //1. Process only the first K elements
-    int i;
-
-    for(i=0;i<k;i++){
-        //Logic here ...
-
-        while(!Q.empty() && a[i] > a[Q.back()]){
-            Q.pop_back();
-        }
-
-        Q.push_back(i);
-
-    }
-
-    //2. Remaining the elements of the array
-    for( ;i<n;i++){
-        cout << a[Q.front()]<<" ";
-        //Remove element from the left (contraction when an index lies outside the current window)
-        while(!Q.empty() and Q.front() <=i-k){
-            Q.pop_front();
-        }
-        while(!Q.empty() && a[i] >= a[Q.back()]){
-            Q.pop_back();
-        }
-
-        //always
-        Q.push_back(i);
-    }
-
-
-    cout<< a[Q.front()];
-
+void maxSubArrayK(vector<int> &nums, int k)
+{
+  vector<int> ans;
+  deque<int> dq;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    if (!dq.empty() && dq.front() == i - k)
+      dq.pop_front();
+    while (!dq.empty() && nums[dq.back()] < nums[i])
+      dq.pop_back();
+    dq.push_back(i);
+    if (i >= k - 1)
+      ans.push_back(nums[dq.front()]);
+  }
+  for (int i : ans)
+  {
+    cout << i << " ";
+  }
 }
 
-int main() {
-    
-    vector<int> arr{1,2,3,1,4,5,2,3,5};
-    int k = 3;
-
-    maxSubArrayK(arr,k);
-
-
-    
-    
-    return 0;
+int main()
+{
+  vector<int> nums{1, 3, -1, -3, 5, 3, 6, 7};
+  int k = 3;
+  maxSubArrayK(nums, k);
+  return 0;
 }
