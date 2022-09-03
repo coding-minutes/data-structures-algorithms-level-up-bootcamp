17b177a028a6b491c37dd969846e9f2bf4d96327
Mrityunjay Kumar <46307002+mrityunjay33@users.noreply.github.com>
2022-09-04 03:11:39 +0530

    Update 06 longest_band.cpp
    Most optimised : O(n)

--- 01 Arrays & Vectors/06 longest_band.cpp
+++ 01 Arrays & Vectors/06 longest_band.cpp
@@ -4,38 +4,35 @@
 using namespace std;
 
 int largestBand(vector<int> arr){
-	int n = arr.size();
-	unordered_set<int> s;
+        int n = arr.size();
+	    unordered_set<int> s;
 	
-	//Data inside a set
-	for(int x : arr){
-		s.insert(x);
-	}
+	    //Data inside a set
+	    for(int x : arr){
+		    s.insert(x);
+	    }
+	
+        //Iterate over the arr
+        int largestLen = 1;
+        for(auto element : s){
+            int parent = element - 1;
+            if(s.find(parent)==s.end()){
+		    
+                //find entire band / chain starting from element
+                int next_no = element + 1;
+                int cnt = 1;
 
-	//Iterate over the arr
-	int largestLen = 1;
-
-	for(auto element : s){
-		int parent = element - 1;
-
-		if(s.find(parent)==s.end()){
-			//find entire band / chain starting from element
-			int next_no = element + 1;
-			int cnt = 1;
-
-			while(s.find(next_no)!=s.end()){
-				next_no++;
-				cnt++;
-			}
-
-			if(cnt>largestLen){
-				largestLen = cnt;
-			}
-		}
-	}
-
-
-	return largestLen;
+                while(s.find(next_no)!=s.end()){
+                    next_no++;
+                    cnt++;
+                }
+                if(cnt>largestLen){
+                    largestLen = cnt;
+                }
+            }
+        }
+        return n>0 ? largestLen:0;
+    }
 }
 
 
